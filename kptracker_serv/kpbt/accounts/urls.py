36f9bc69f4e188e39a88c43fc37893cb355cadc1
Cody Trombley <cjtrombley@oakland.edu>
2019-10-02 21:58:14 -0400

    Merge finalization

--- kptracker_serv/kpbt/accounts/urls.py
+++ kptracker_serv/kpbt/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from kpbt.accounts import views
 
 urlpatterns = [
-	path('register/', views.signup, name="register"),
+	path('register/', views.register, name="register"),
 	path('create_profile/', views.create_profile, name='create_profile'),
 	path('view_profile/', views.view_profile, name='view_profile'),
 ]
